a79056b5411e2531cee4c93c30b269be410dc7cc
michaelphamct <47987943+michaelphamct@users.noreply.github.com>
2019-12-13 09:54:40 -0500

    Update InstructionsActivity.java

--- app/src/InstructionsActivity.java
+++ app/src/InstructionsActivity.java
@@ -2,7 +2,6 @@ package com.example.djrai;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -18,8 +17,7 @@ public class InstructionsActivity extends AppCompatActivity {
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent returnIntent = new Intent(InstructionsActivity.this, MainActivity.class);
-                startActivity(returnIntent);
+                InstructionsActivity.super.onBackPressed();
             }
         });
     }
