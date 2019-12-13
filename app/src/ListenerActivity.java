73884af5df0fe4ffd3f7d1b59b9ab8a31f7c2330
michaelphamct <47987943+michaelphamct@users.noreply.github.com>
2019-12-12 21:18:19 -0500

    Fix update skip count and update dj vote count

--- app/src/ListenerActivity.java
+++ app/src/ListenerActivity.java
@@ -3,12 +3,71 @@ package com.example.djrai;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ListenerActivity extends AppCompatActivity {
+
+    ListView lv;
+    List<String> list;
+    Button skip;
+    Button newDJ;
+    boolean newDJRequested = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listener);
+
+        skip = (Button)findViewById(R.id.skip_button);
+        newDJ = (Button)findViewById(R.id.newdj_button);
+
+        list = new ArrayList<String>();
+        list.add("\"Despacito\" by Luis Fonsi");
+        list.add("\"Video Games\" by Lana Del Rey");
+        list.add("\"All I Want for Christmas\" by Mariah Carey");
+
+        lv = (ListView)findViewById(R.id.song_listview2);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,list);
+        lv.setAdapter(arrayAdapter);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String name = parent.getItemAtPosition(position).toString();
+                Toast.makeText(getApplicationContext(), name, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        skip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivity.updateSkipVotes();
+            }
+        });
+
+        newDJ.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!newDJRequested)
+                {
+                    MainActivity.updateNewDJVotes();
+                    newDJRequested = true;
+                    newDJ.setEnabled(false);
+                    Toast toast = Toast.makeText(getApplicationContext(),
+                            "You can only request a new DJ once",
+                            Toast.LENGTH_SHORT);
+
+                    toast.show();
+                }
+            }
+        });
     }
 }
